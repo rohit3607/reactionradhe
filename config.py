6795c5480985831dc17e64374f771952705fbd53
rohit3607 <rohitplayer87089@gmail.com>
2025-06-26 22:48:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "8027707636:AAF0b5F4u2_ufNBddDi8iOtmjd8YX0h7Eqc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", "6949414014"))
+OWNER_ID = int(getenv("OWNER_ID", "8181241262"))
 SUPPORT_GRP = "SuperToppers0"
 UPDATE_CHNL = "SuperToppers"
 OWNER_USERNAME = "UncleChipssBot"
